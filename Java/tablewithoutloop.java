9384454f07e252901738052e381d090355451ba9
Kunal Maity <kunalmaity075@gmail.com>
2026-08-22 12:17:59 +0530

    Add java practice

---
+++ Java/tablewithoutloop.java
@@ -0,0 +1,22 @@
+// Make a program that prints the table of a number that is input by the user. 
+// (HINT - You will have to write 10 lines for this but as we proceed in the course you will be studying about ‘LOOPS’ that will simplify your work A LOT!)
+
+import java.util.Scanner;
+
+public class tablewithoutloop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N= sc.nextInt();
+        System.out.println( N*1);
+        System.out.println( N*2);
+        System.out.println( N*3);
+        System.out.println( N*4);
+        System.out.println( N*5);
+        System.out.println( N*6);
+        System.out.println( N*7);
+        System.out.println( N*8);
+        System.out.println( N*9);
+        System.out.println( N*10);
+    }
+    
+}
